4ea250e2554540462fad1b0146a8574a5777fba3
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-16 23:04:27 +0100

    Starting with Arrays

---
+++ 01.Introduction/Approach.js
@@ -0,0 +1,10 @@
+// Breaking problem into small tasks
+
+
+/* Approach: Three stages:
+    1. Beginning stage : Mechanics of programing (Knowing exactly what is happening in technical perspective)
+        Think like a computer . 
+    2. Middle stage : Understand existing programs (Understand code. Others people programs)
+        
+    1. Beginning stage : Mechanics of programing (Knowing exactly what is happening in technical perspective)
+ */
