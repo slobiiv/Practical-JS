86900fa422bd226e18c3c3d461d96036adf45557
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-19 01:48:45 +0100

    Functions are like recipes

---
+++ 03.Functions/01.Functions-are-just recipes.js
@@ -0,0 +1,11 @@
+/* 
+makeTurkeySandwich
+  Get one slice of bread.
+  Add turkey.
+  Put a slice of bread on top. 
+*/
+
+function makeTurkeySandwich() {
+  console.log('Get one slice of bread - Add turkey - Put a slice of bread on top.'); 
+}
+makeTurkeySandwich(); // runs the function
---
+++ 03.Functions/02.Customize-functions.js
@@ -0,0 +1,22 @@
+/* makeSandwichWith_____ 
+  Get one slice of bread.
+  Add _____.
+  Put a slice of bread on top.
+ */
+// Customizing functions
+
+  function makeSandwichWith(filling) { // we have a filling as a parameter
+    console.log('Get one slice of bread');
+    console.log('Add ' + filling);
+    console.log('Put a slice of bread on top.');
+  }
+  makeSandwichWith('turkey'); // and 'turkey' as argument of filling
+  makeSandwichWith('ham');
+  makeSandwichWith('avocado');
+
+
+  //  Continuation
+  function sayHiTo(person) {
+    console.log('hi', person);
+  }
+  sayHiTo('Relja'); // 'Relja' is assigned to a variable 'person'. person = 'Relja';
