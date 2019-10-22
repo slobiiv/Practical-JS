56132caf427589051b8cc5dcd777c5a995e19e86
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-22 23:44:49 +0100

    Objects

---
+++ 05.Objects/01.Introduction.js
@@ -0,0 +1,17 @@
+/*  
+  group of data
+  
+    operatingSystem : windows10
+    screenSize 15.6
+    purchaseYear 2018 
+  
+*/
+
+var myComputer = {
+  operatingSystem: 'windows10',
+  screenSize: '15 inches',
+  purchaseYear: 2018
+};
+
+myComputer.operatingSystem; // windows10
+myComputer; // {operatingSystem: "windows10", screenSize: "15 inches", purchaseYear: 2018}
