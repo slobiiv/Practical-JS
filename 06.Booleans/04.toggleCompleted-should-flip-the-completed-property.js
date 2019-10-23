94b36c467e06a58cd7fa7e4505489858daedbb0f
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-23 23:16:30 +0100

    Finished. Interesting

---
+++ 06.Booleans/04.toggleCompleted-should-flip-the-completed-property.js
@@ -0,0 +1,26 @@
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    console.log('My todos: ', this.todos);
+  },
+  addTodos: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText; 
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;  // if false change to true, and opposite (!)
+    this.displayTodos();
+  }
+}
