25c19d779d42cdf3c356cc0a3fc863ef2007abde
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-26 08:15:17 +0100

    Loops

---
+++ 07.Loops-of-Logic/01.The-For-loop.js
@@ -0,0 +1,17 @@
+/* 
+  i = 0                initialization
+  Say 'hey' if i < 3   condition
+  Increase i by 1      final-expression
+
+  0'hey'
+  1'hey'
+  2'hey'
+  3
+
+  for(initialization, condition, final-expression) {
+
+  }
+*/
+for(var i = 0;i < 3; i++) { // i++ = i + 1
+    console.log('hey');
+}
