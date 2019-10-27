cfc1751e2d8a4924e8604c75a7cb1a1dbe935fb3
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-27 23:38:59 +0000

    Looping

---
+++ 07.Loops-of-Logic/04.displayTodos-should-show-todoText.js
@@ -0,0 +1,33 @@
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    if(this.todos[] = '') {
+      console.log('Your Todo List is empty');
+    } else {
+      console.log('My Todos: ');
+      for(var i = 0; i < this.todos.length;i++) {
+        console.log('My todos: ', this.todos[i].todoText);
+      }
+    }
+  },
+  addTodos: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText; 
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;  // if false change to true, and opposite (!)
+    this.displayTodos();
+  }
+}
