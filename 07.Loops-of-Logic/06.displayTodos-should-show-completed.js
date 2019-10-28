2bf54ebc88ac0cda43532d0fe8f1c02cdb793254
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-28 00:52:27 +0000

    commit

--- 07.Loops-of-Logic/06.displayTodos-should-show-completed.js
+++ 07.Loops-of-Logic/06.displayTodos-should-show-completed.js
@@ -2,7 +2,7 @@ var todoList = {
   todos: [],
   displayTodos: function() {
     if(this.todos.length === 0 ) {
-      console.log('Your Todo list is empty')
+      console.log('Your Todo list is empty');
     } else {
       console.log('My Todos: ');
       for(var i = 0; i < this.todos.length;i++) {
