192786e30e76fa8c7819345b53e346d0cec1ed57
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-29 00:00:12 +0000

    how are primitives and objects assigned to a variables

---
+++ 09.Data-types-and-comparisons/How-are-primitives-and-objects-stored-inside-of-variables/example01.js
@@ -0,0 +1,6 @@
+var myPrimitive = 10;
+var myObject = {name: 'gordon'};
+/* 
+myPrimitive = 10
+myObject = Memory address --> {name : 'gordon'}
+*/
---
+++ 09.Data-types-and-comparisons/How-are-primitives-and-objects-stored-inside-of-variables/example02.js
@@ -0,0 +1,6 @@
+var myHouse = {color: 'blue'};
+myHouse.color = 'red';7
+/* 
+  myHouse = Memory address  --> {color: 'blue'} (reassigned)
+                            --> {color: 'red'}  
+*/                         
