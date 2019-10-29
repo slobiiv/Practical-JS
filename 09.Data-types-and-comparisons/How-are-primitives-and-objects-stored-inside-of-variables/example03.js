bed0676e1406dd031ce4d10c50263e4ff2d85b11
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-29 00:08:40 +0000

    commit examples of primitives and objects are stored in variables

---
+++ 09.Data-types-and-comparisons/How-are-primitives-and-objects-stored-inside-of-variables/example03.js
@@ -0,0 +1,10 @@
+var myHouse = {color : 'blue'};
+var color = myHouse.color;
+color = 'red';
+
+/* 
+myHouse = Memory address --> {color : 'blue'}
+color =  'blue' --> 'red' (updated)
+
+myHouse and color are two different variables
+*/
