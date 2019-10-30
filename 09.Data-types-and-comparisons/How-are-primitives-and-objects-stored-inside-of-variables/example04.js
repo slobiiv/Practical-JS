dc8a8dd7f0736886d5be9b80c14592b26e0131df
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-30 00:27:54 +0000

    example4,5

---
+++ 09.Data-types-and-comparisons/How-are-primitives-and-objects-stored-inside-of-variables/example04.js
@@ -0,0 +1,12 @@
+var myHouse1 = {color: 'blue'};
+var myHouse2 = myHouse1;
+myHouse2.color = 'red';
+
+/* 
+myHouse1  memory address 1  --> color: 'blue' 
+myHouse2  memory address 1  --> color: 'blue'  --> 'red'
+*/
+
+
+/* myHouse1.color  = 'red' */
+/* myHouse2.color  = 'red' */
---
+++ 09.Data-types-and-comparisons/How-are-primitives-and-objects-stored-inside-of-variables/example05.js
@@ -0,0 +1,9 @@
+var myHouse1 = {color: 'blue'};
+var myHouse2 = {color: 'blue'};
+myHouse2.color = 'red';
+
+/* 
+  myHouse1 = memory address 1 --> color: 'blue'
+  myHouse2 = memory address 2 --> color: 'blue'
+                              --> color: 'red'
+*/
