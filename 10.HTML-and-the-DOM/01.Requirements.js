cbd94b814acab713e719903e47f8c2198229e1ac
Slobodan Vlaic <slobiiv@gmail.com>
2019-10-30 22:24:58 +0000

    DOM

---
+++ 10.HTML-and-the-DOM/01.Requirements.js
@@ -0,0 +1,5 @@
+/* 
+- There should be a 'Display Todos' button and a 'Togglea all' button in the app 
+- Clicking 'Display todos' should tun todoList.displayTodos.
+- CLicking 'Toggle all' should run todoList.toggleAll
+*/
---
+++ 10.HTML-and-the-DOM/03.What-is-the-DOM.js
@@ -0,0 +1,4 @@
+/* 
+  DOM(Document object model) is a browsers understanding of what it thinks an HTML page is.
+  The nodes of every document are organized in a tree structuce, called the DOM tree.
+*/
